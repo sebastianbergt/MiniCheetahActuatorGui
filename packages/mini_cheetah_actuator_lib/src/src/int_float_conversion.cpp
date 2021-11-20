e305633786d3883176bc36efb1bdced15842814a
Sebastian Bergt <sebastian@bergt.org>
2021-11-20 22:44:28 +0100

    int to float

--- packages/mini_cheetah_actuator_lib/src/src/int_float_conversion.cpp
+++ packages/mini_cheetah_actuator_lib/src/src/int_float_conversion.cpp
@@ -10,7 +10,7 @@ std::uint32_t toInt(float x, float x_min, float x_max, std::uint32_t bits) {
 
   const auto x_delta = x_clamped - x_min;
 
-  const std::uint32_t maximum_int_value = (1 << bits) - 1;
+  const float maximum_int_value = (1 << bits) - 1;
   const float value_of_lsb = maximum_int_value / span;
 
   return x_delta * value_of_lsb;
@@ -19,7 +19,7 @@ std::uint32_t toInt(float x, float x_min, float x_max, std::uint32_t bits) {
 float toFloat(std::uint32_t x, float x_min, float x_max, std::uint32_t bits) {
   const auto span = x_max - x_min;
 
-  const std::uint32_t maximum_int_value = (1 << bits) - 1;
+  const float maximum_int_value = (1 << bits) - 1;
   const float inverse_value_of_lsb = span / maximum_int_value;
 
   return x * inverse_value_of_lsb + x_min;
